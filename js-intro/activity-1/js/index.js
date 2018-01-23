3a6bc5bb7edd29d878242618eab0688bcf74298b
Blake Franzen <bef6823@gmail.com>
2018-01-23 09:33:16 -0800

    added changes

--- js-intro/activity-1/js/index.js
+++ js-intro/activity-1/js/index.js
@@ -1,27 +1,31 @@
 // Main JavaScript File: enable strict mode
-
+'use strict';
 
 /* ******************** Basic variables and methods ******************* */
 
 // Create a string variable, then print it out to the console ("log" it)
-
+let str = 'Hello World';
+console.log(str);
 
 // Use the length property to see how many characters are in your `str` variable
 // Log the result
-
+let len = str.length;
+console.log(len);
 
 // Use the `toUpperCase` method to create a new variable `upperStr` that has the `str` value, but all UPPPERCASE.
-
-
+let upperStr = str.toUpperCase();
+console.log(upperStr);
 // Use the "split" method create a new variable `words` that is an array of the component words of `str`
-
+let words = str.split(' ');
+console.log(words);
 
 // Use the length property to see how many words are in your `words` array
 // Log your result
-
+let wordLen = words.length;
+console.log(wordLen);
 
 // Create an array "breakfast" of everything that you ate for breakfast
-
+let breakfast = ['']
 
 // Log the phrase 'I had N items for breakfast'
 

7fde53b4d1a861da20c2790866da6cbc2e594bcf
Blake Franzen <bef6823@gmail.com>
2018-01-26 18:52:02 -0800

    pushing changes

--- js-intro/activity-2/js/index.js
+++ js-intro/activity-2/js/index.js
@@ -17,10 +17,34 @@ ctx.strokeStyle = 2;
 let input = document.getElementById('textInput');
 input.onkeydown = drawLines;
 
+const directions = [
+    { x: 10, y: 0 },
+    { x: 0, y: 10 },
+    { x: -10, y: 0 },
+    { x: 0, y: -10 }
+];
 
 // Function to draw lines
 function drawLines() {
-
+    let pos = {
+        x: width / 2,
+        y: height / 2
+    };
+    ctx.clearRect(0, 0, 500, 500);
+    ctx.beginPath();
+    ctx.moveTo(pos.x, pos.y);
+    let words = input.value.split(' ');
+    let wordLens = words.map(function (d) {
+        return d.length;
+    });
+    wordLens.forEach(function (d, i) {
+        let dirIndex = i % 4;
+        let currDir = directions[dirIndex];
+        pos.x += (currDir.x * d);
+        pos.y += (currDir.y * d);
+        ctx.lineTo(pos.x, pos.y);
+        ctx.stroke();
+    });
 }
 
 
